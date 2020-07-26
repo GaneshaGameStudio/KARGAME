6d12cd7af8863121f824e03e8d4e950bdc8c7220
A.Run <arundhaduti@gmail.com>
2020-07-27 00:20:07 +0530

    Cleaned up and added roof color tag

--- Scripts/buildingColorAdder.py
+++ Scripts/buildingColorAdder.py
@@ -1,11 +1,14 @@
+import sys
+import os
 from xml.etree import ElementTree
 import xml.etree.ElementTree as et
 import random
 
-buildingColors = ("#FFBBAD","#DFBFDE","#F4E9AC","#A6E0F8","#698A7D","#FF934D","#BB9BBA","#D8CE97","#6CA3BA")
+buildingColors = ("#FFBBAD","#DFBFDE","#F4E9AC","#A6E0F8","#698A7D","#FF934D","#BB9BBA","#D8CE97","#6CA3BA","#ECECEC","#8BAA4E")
+roofColors = ("#93897E","#CACACA","#848484")
 
-# actual map.osm data from Openstreetmap which is imported through blender - use this as input file
-tree = et.parse("F:\Blender\YashaswiOSM\map_5.osm")
+# actual map.osm data from Openstreetmap which is imported through blender - use this as input file below
+tree = et.parse(sys.path[0]+"\map_5.osm")
 root = tree.getroot()
 
 count = 0
@@ -17,15 +20,23 @@ for e in tree.findall('.//way'):
     if tagSize==1:
         colorSize = len(e.findall("./tag[@k='building:colour']"))
         if colorSize==0:
-            #generate random number and get the random color from the tuple
-            rand = random.randint(0,8)
+            #generate random number and get the random color from the tuples
+            rand = random.randint(0,10)
             color = buildingColors[rand]
+            roofRand = random.randint(0,2)
+            roofColor = roofColors[roofRand]
 
-            thisdict = {"k":"building:colour","v":color}
-            ElementTree.SubElement(e,"tag",thisdict)
+            buildingColor = {"k":"building:colour","v":color}
+            ElementTree.SubElement(e,"tag",buildingColor)
+
+            roofCol = {"k":"roof:colour","v":roofColor}
+            ElementTree.SubElement(e, "tag", roofCol)
 
 # uncomment below line to print the edited xml to console
 # et.dump(root)
 
-# writes it as a new file
-tree.write("F:\Blender\YashaswiOSM\Output\map5.xml")
+if not os.path.exists('ScriptOutput'):
+    os.makedirs('ScriptOutput')
+
+# writes it as a new file in ScriptOutput folder
+tree.write(sys.path[0]+"\ScriptOutput\map5.osm")
