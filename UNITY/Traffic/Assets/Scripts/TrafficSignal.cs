253c67b83871b2628096e7fa0704aaba105f3836
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2020-10-14 18:47:09 +0200

    Traffic signal logic

---
+++ UNITY/Traffic/Assets/Scripts/TrafficSignal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficSignal : MonoBehaviour
+{   public float signaltime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine(signaltime));
+    }
+    IEnumerator ExampleCoroutine(float signaltime)
+    {   
+        while(Time.deltaTime>0){
+            foreach(Transform child in transform)
+            {   
+                Collider col = child.GetComponent<Collider>();
+                col.enabled = false;
+                yield return new WaitForSeconds(signaltime);
+                col.enabled = true;
+                yield return new WaitForSeconds(signaltime*0.2f);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
