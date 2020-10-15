c1ff421d5b61fa29b665b527e3fb88a290279a1c
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2020-10-15 19:14:57 +0200

    Traffic logic UNITY complete

--- Blender_elements/World/StreetStuff/Street.md
+++
@@ -1,5 +0,0 @@
-All street related parts go here.
-
-Order of colors in PNG:
-1st line - electricity poles, lamp poles.
-2nd line - Lamps
--- UNITY/Traffic/Assets/Scripts/TrafficSignal.cs
+++ UNITY/Traffic/Assets/Scripts/TrafficSignal.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrafficSignal : MonoBehaviour
-{   public float signaltime;
-    
+{   
+    public float signaltime;
+    public Texture2D Rtexture;
+    public Texture2D Gtexture;
+    public Renderer[] Signal;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +16,17 @@ public class TrafficSignal : MonoBehaviour
     IEnumerator ExampleCoroutine(float signaltime)
     {   
         while(Time.deltaTime>0){
+            int i = 0;
             foreach(Transform child in transform)
             {   
                 Collider col = child.GetComponent<Collider>();
                 col.enabled = false;
+                Signal[i].material.mainTexture = Gtexture;
                 yield return new WaitForSeconds(signaltime);
                 col.enabled = true;
+                Signal[i].material.mainTexture = Rtexture;
                 yield return new WaitForSeconds(signaltime*0.2f);
+                i++;
             }
         }
     }
