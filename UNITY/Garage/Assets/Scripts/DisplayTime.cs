dae14e946730ce1bb3264b0a681c1d6db2fa2935
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2020-10-23 01:38:25 +0200

    UNITY garage time and weather temperature

---
+++ UNITY/Garage/Assets/Scripts/DisplayTime.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Xml;
+using TMPro;
+using UnityEngine.UI;
+using System.IO;
+
+public class DisplayTime : MonoBehaviour
+{   
+    private const string API_KEY = "42d4191ee9409cb50f90142a4630a078";
+    private const string CurrentUrl =
+        "http://api.openweathermap.org/data/2.5/weather?" +
+        "q=Bengaluru,IN@&mode=xml&units=metric&APPID=" + API_KEY;
+    private const string ForecastUrl =
+        "http://api.openweathermap.org/data/2.5/forecast?" +
+        "q=Bengaluru,IN&mode=xml&units=metric&APPID=" + API_KEY;
+    public TextMeshProUGUI TextPro;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        StartCoroutine(GetTimeRequest("http://worldtimeapi.org/api/timezone/Asia/Kolkata.txt"));
+    }
+    
+    IEnumerator GetTimeRequest(string uri)
+    {   
+        while (true){
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+            {   
+                
+                // Request and wait for the desired page.
+                yield return webRequest.SendWebRequest();
+                
+                string Time = webRequest.downloadHandler.text;
+                char[] delimiterChars = { 'T', '.'};
+                using (StringReader reader = new StringReader(Time))
+                {   
+                    int iter =0;
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {   
+                        if(iter==2){
+                            string[] words = Time.Split(delimiterChars);
+                            string TimeDisplay = words[5].Substring(0,5);
+                            TextPro.SetText(TimeDisplay);
+                            break;
+                        }
+                        iter++;
+                    }
+                }
+            yield return new WaitForSeconds(1f);
+            }
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UNITY/Garage/Assets/Scripts/DisplayWeather.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Xml;
+using TMPro;
+using UnityEngine.UI;
+using System.IO;
+
+public class DisplayWeather : MonoBehaviour
+{   
+    private const string API_KEY = "42d4191ee9409cb50f90142a4630a078";
+    private const string CurrentUrl =
+        "http://api.openweathermap.org/data/2.5/weather?" +
+        "q=Bengaluru,IN@&mode=xml&units=metric&APPID=" + API_KEY;
+    private const string ForecastUrl =
+        "http://api.openweathermap.org/data/2.5/forecast?" +
+        "q=Bengaluru,IN&mode=xml&units=metric&APPID=" + API_KEY;
+    public TextMeshProUGUI TextPro;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        StartCoroutine(GetWeatherRequest(CurrentUrl));
+    }
+    IEnumerator GetWeatherRequest(string uri)
+    {   
+        while(true){
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+                {
+                    // Request and wait for the desired page.
+                    yield return webRequest.SendWebRequest();
+
+                    string[] pages = uri.Split('/');
+                    int page = pages.Length - 1;
+                    
+                    string Weather = webRequest.downloadHandler.text;
+                    string TempDisplay = DispWeather(Weather);
+                    TextPro.SetText(TempDisplay + "ö");
+                    yield return new WaitForSeconds(1800f);
+            
+                }
+            }
+    }
+    
+    private string DispWeather(string xml)
+    {
+        // Load the response into an XML document.
+        XmlDocument xml_doc = new XmlDocument();
+        xml_doc.LoadXml(xml);
+        string temperature = "";
+        foreach (XmlNode time_node in xml_doc.SelectNodes("current"))
+        {
+            XmlNode temp_node = time_node.SelectSingleNode("temperature");
+            temperature = temp_node.Attributes["value"].Value;
+            
+        }
+        return temperature;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
