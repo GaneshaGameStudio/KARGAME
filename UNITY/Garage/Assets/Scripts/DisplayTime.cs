33da8ae3f738f841a56259c98b597632228aa79a
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2020-12-23 05:20:54 +0100

    time derived from system directly

--- UNITY/Garage/Assets/Scripts/DisplayTime.cs
+++ UNITY/Garage/Assets/Scripts/DisplayTime.cs
@@ -6,6 +6,7 @@ using System.Xml;
 using TMPro;
 using UnityEngine.UI;
 using System.IO;
+using System;
 
 public class DisplayTime : MonoBehaviour
 {   
@@ -14,47 +15,27 @@ public class DisplayTime : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   
-        StartCoroutine(GetTimeRequest("http://worldtimeapi.org/api/timezone/Asia/Kolkata.txt"));
+        StartCoroutine(GetTimeRequest());
     }
     
-    IEnumerator GetTimeRequest(string uri)
+    IEnumerator GetTimeRequest()
     {   
         while (true){
-            using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
-            {   
-                
-                // Request and wait for the desired page.
-                yield return webRequest.SendWebRequest();
-                
-                string Time = webRequest.downloadHandler.text;
-                char[] delimiterChars = { 'T', '.'};
-                char[] delimiterCharstime = { ':'};
-                using (StringReader reader = new StringReader(Time))
-                {   
-                    int iter =0;
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {   
-                        if(iter==2){
-                            string[] words = Time.Split(delimiterChars);
-                            string TimeDisplay = words[5].Substring(0,5);
-                            string[] wordstime = TimeDisplay.Split(delimiterCharstime);
-                            TextPro.SetText(wordstime[0]+" : "+wordstime[1]);
-                            if(int.Parse(wordstime[0])<7 || int.Parse(wordstime[0])>=17){
-                                Camera.main.GetComponent<Lights>().LightOn = true;
-                            }
-                            else{
-                                Camera.main.GetComponent<Lights>().LightOn = false;
-                            }
-                            int currenttime = (int.Parse(wordstime[0]))*60 + int.Parse(wordstime[1]);
-                            GameObject.Find("Directional Light").GetComponent<SunPosition>().Timecurrent = currenttime;
-                            break;
-                        }
-                        iter++;
-                    }
+            char[] delimiterChars = { ' ', ':'};
+            string Time = DateTime.Now.ToString("mm/dd/yyyy HH:mm:ss");
+            string[] wordstime = Time.Split(delimiterChars);
+            //print(words[1]);
+            //print(words[2]);
+            TextPro.SetText(wordstime[1]+" : "+wordstime[2]);
+            if(int.Parse(wordstime[1])<7 || int.Parse(wordstime[2])>=17){
+                    Camera.main.GetComponent<Lights>().LightOn = true;
+                 }
+            else{
+                    Camera.main.GetComponent<Lights>().LightOn = false;
                 }
-            yield return new WaitForSeconds(1f);
-            }
+                int currenttime = (int.Parse(wordstime[1]))*60 + int.Parse(wordstime[2]);
+                GameObject.Find("Directional Light").GetComponent<SunPosition>().Timecurrent = currenttime;
+                yield return new WaitForSeconds(1f);
         }
     }
     
