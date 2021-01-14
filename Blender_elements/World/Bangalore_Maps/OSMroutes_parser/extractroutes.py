9fbb2542bc6e7ecd9f68de16dfc84fc1642adca5
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2021-01-15 00:27:15 +0100

    bus routes - INIT framework

---
+++ Blender_elements/World/Bangalore_Maps/OSMroutes_parser/extractroutes.py
@@ -0,0 +1,36 @@
+import os
+import bpy
+import csv
+
+filepath = bpy.data.filepath
+print(filepath)
+directory = os.path.dirname(filepath) + "/bus_routes/"
+for filename in os.listdir(directory):
+    if filename.endswith(".csv"): 
+        print(filename.split(".")[0])
+        #create parent object here
+        if not(bpy.context.scene.objects.get(filename.split(".")[0])):
+            print(filename.split(".")[0])
+            bpy.ops.mesh.primitive_cube_add()
+            par = bpy.context.selected_objects[0]
+            # change name
+            par.name = filename.split(".")[0]
+            bpy.ops.object.mode_set(mode="EDIT")
+            bpy.ops.mesh.select_all(action="SELECT")
+            bpy.ops.mesh.delete(type='VERT')
+            bpy.ops.object.mode_set(mode="OBJECT")
+            i = 0
+            with open(directory + filename, "rt", encoding='ascii') as infile:
+                read = csv.reader(infile)
+                for row in read :
+                    bpy.ops.mesh.primitive_cube_add()
+                    chld = bpy.context.selected_objects[0]
+                    chld.name = filename.split(".")[0] + "-Cube" + str(i)
+                    chld.location =(float(row[0]),float(row[1]),0)
+                    chld.parent = par
+                    bpy.ops.object.select_all(action='DESELECT')
+                    i+=1
+    else:
+        continue
+    
+    #break
---
+++ Blender_elements/World/Bangalore_Maps/OSMroutes_parser/parserelations.py
@@ -0,0 +1,74 @@
+import lxml.etree
+import math
+import os
+if not os.path.exists('bus_routes'):
+    os.makedirs('bus_routes')
+
+earthradius = 6371
+org_coord=[13.0151,77.5548,13.0157,77.5555] # window
+
+def haversine(x1,y1,x2,y2):
+    havlat = math.sin(math.radians((x1-x2)/2))**2
+    havlong = math.sin(math.radians((y1-y2)/2))**2 * math.cos(math.radians(x1))*math.cos(math.radians(x2))
+    
+    c = 2 * math.atan2((havlat + havlong)**0.5,(1 - havlat + havlong)**0.5)
+    return 1000*earthradius*c
+
+totalwide = haversine(org_coord[0],org_coord[1],org_coord[0],org_coord[3]) 
+totalheight = haversine(org_coord[0],org_coord[1],org_coord[2],org_coord[1])
+print(totalwide)
+print(totalheight)
+org_dist=[-totalwide/2.,-totalheight/2.,totalwide/2.,totalheight/2.] #viewport
+sx = (org_dist[2]-org_dist[0])/(org_coord[2]-org_coord[0])
+sy = (org_dist[3]-org_dist[1])/(org_coord[3]-org_coord[1])
+
+def convertcoords(lat,lon):
+    myfile.write(str(org_dist[0] + (lat - org_coord[0])*sx) + "," +str(org_dist[1] + (lon - org_coord[1])*sy) + "\n")
+    return 
+
+def getcoords(node,typ):
+    for ndcoord in tree.findall('//node[@id="'+str(node)+'"]'):
+        lat = ndcoord.get('lat')
+        lon = ndcoord.get('lon')
+        convertcoords(float(lat),float(lon))
+        #print(round(latd,6),round(lond,6),typ)
+
+def getwayid():
+    for member in tag.getparent():
+        if(member.get('type') == "node"):
+            noderef = member.get('ref')
+            member.get('role')
+            getcoords(noderef,member.get('role'))
+        else:
+            wayref = member.get('ref')
+            for wyref in tree.findall('//way[@id="'+str(wayref)+'"]'):
+                for noderef in wyref:
+                    getcoords(noderef.get('ref'),member.get('role'))
+
+tree = lxml.etree.parse('map-3.xml')
+for tag in tree.findall('//relation//tag[@k="operator"][@v="BMTC"]'):
+    routenumbercheck = tag.getprevious().getprevious().get('k')
+    if(routenumbercheck == "name"):
+        print(tag.getprevious().getprevious().get('v'))
+        if not os.path.exists('bus_routes/'+str(tag.getprevious().getprevious().get('v'))+'.csv'):
+            with open('bus_routes/'+str(tag.getprevious().getprevious().get('v'))+'.csv', 'a') as myfile: 
+                getwayid()
+        else:
+            os.remove('bus_routes/'+str(tag.getprevious().getprevious().get('v'))+'.csv')
+            with open('bus_routes/'+str(tag.getprevious().getprevious().get('v'))+'.csv', 'a') as myfile: 
+                getwayid()
+
+    else:
+        print(tag.getprevious().getprevious().getprevious().get('v'))
+        if not os.path.exists('bus_routes/'+str(tag.getprevious().getprevious().getprevious().get('v'))+'.csv'):
+            with open('bus_routes/'+str(tag.getprevious().getprevious().getprevious().get('v'))+'.csv', 'a') as myfile: 
+                getwayid()
+        else:
+            os.remove('bus_routes/'+str(tag.getprevious().getprevious().get('v'))+'.csv')
+            with open('bus_routes/'+str(tag.getprevious().getprevious().get('v'))+'.csv', 'a') as myfile: 
+                getwayid()
+    #print("relation id: " + str(tag.getparent().get('id')))
+    #break
+
+
+
