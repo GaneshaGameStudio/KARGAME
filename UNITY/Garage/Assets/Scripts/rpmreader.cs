dd5530b52fc71f0a106c16dc96ade96f95474d42
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2021-01-19 20:08:19 +0100

    Changed scaling for 4wheeler rpm reader

--- UNITY/Garage/Assets/Scripts/rpmreader.cs
+++ UNITY/Garage/Assets/Scripts/rpmreader.cs
@@ -22,8 +22,17 @@ public class rpmreader : MonoBehaviour
         GO = GameObject.Find(VehicleID.Vehicle + "(Clone)");
         rb = GO.GetComponent<Rigidbody>();
         wheelradius = GameObject.FindWithTag("WheelFC").GetComponent<WheelCollider>().radius;
+        print(wheelradius);
         float currentrpm = rb.velocity.magnitude*3.6f/(wheelradius*0.10472f);
-        float normrpm = Mathf.Pow((currentrpm*0.015f),2)/rpmmax;
-        GetComponent<RectTransform>().sizeDelta = new Vector2(Mathf.Max(10f,Mathf.Min(Mathf.Log(normrpm)*rectwidth,rectwidth)), 15f);
+        
+        if(GO.tag=="4Wheeler" || GO.tag=="6Wheeler"){
+            float normrpm = Mathf.Pow((currentrpm*0.075f),2)/rpmmax;
+            GetComponent<RectTransform>().sizeDelta = new Vector2(Mathf.Max(10f,Mathf.Min(Mathf.Log(normrpm)*rectwidth,rectwidth)), 15f);
+        }
+        else{
+            float normrpm = Mathf.Pow((currentrpm*0.015f),2)/rpmmax;
+            GetComponent<RectTransform>().sizeDelta = new Vector2(Mathf.Max(10f,Mathf.Min(Mathf.Log(normrpm)*rectwidth,rectwidth)), 15f);
+        }
+        
     }
 }
