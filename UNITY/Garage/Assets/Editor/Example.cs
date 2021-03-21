668b4d56028cea773c7aa1f8d133cc8ac36f5fb6
A.Run <arundhaduti@gmail.com>
2021-03-21 15:07:34 +0530

    Updated road prefabs

--- UNITY/Garage/Assets/Editor/Example.cs
+++ UNITY/Garage/Assets/Editor/Example.cs
@@ -30,5 +30,55 @@ public class Example
         }
             
     }
+    [MenuItem("Examples/Create Collider")]
+    static void Createcollider()
+    {
+        
+        GameObject[] folderObjs;
+        folderObjs = null;
+        MeshCollider MC;
+        MeshCollider MCP;
+        PhysicMaterial phymMaterial = null;
+        phymMaterial = (PhysicMaterial)Resources.Load("road");
+
+        folderObjs = Resources.LoadAll<GameObject>("Roads_prefabs");
+
+        //int j = 0;
+
+       // foreach (GameObject pars in folderObjs)
+       for(int k=0; k<folderObjs.Length; k++)
+        {
+            GameObject pars = folderObjs[k];
+           // j++;
+            
+            EditorUtility.SetDirty(pars);
+            pars.AddComponent<MeshCollider>();
+            MCP = pars.GetComponent<MeshCollider>();
+            MCP.sharedMaterial = phymMaterial;
+            //par.AddComponent<MeshCollider>();
+            Debug.Log(pars.name);
+            for (int i = 0; i < pars.transform.childCount; i++){
+                GameObject childobjects = pars.transform.GetChild(i).gameObject;
+                EditorUtility.SetDirty(childobjects);
+                childobjects.AddComponent<MeshCollider>();
+                MC = childobjects.GetComponent<MeshCollider>();
+
+                MC.sharedMaterial = phymMaterial;
+               // Debug.Log(childobjects.name);
+            }
+
+            /*if (j >= 2)
+            {
+                Debug.Log(j);
+                break;
+            }*/
+
+            //PrefabUtility.SaveAsPrefabAsset(pars,"Roads_prefabs");
+            //break;
+            //Destroy(par);
+
+        }
+            
+    }
 
 }
