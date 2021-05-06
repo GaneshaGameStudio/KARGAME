48754f3c6e1b2e343b082d76f9941f8ee756109a
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2021-05-06 03:50:36 +0200

    birdaniation

---
+++ UNITY/Garage/Assets/Scripts/Kaage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kaage : MonoBehaviour
+{   
+    
+    // Start is called before the first frame update
+    void OnEnable()
+    {       
+        InvokeRepeating("Updateposition", 0, 10.0f);
+    }
+    void Updateposition(){
+        
+        transform.position = Camera.main.transform.position + new Vector3 (0f,2.5f,0f);
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {   
+
+    }
+}
