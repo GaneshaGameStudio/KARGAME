85533d37991b570c70434b9b5e10b356b85e5221
A.Run <arundhaduti@gmail.com>
2021-05-25 23:03:20 +0530

    Contains FetchInitDBData, Audio error fix, PP plugin

--- UNITY/Garage/Assets/Scripts/Init.cs
+++ UNITY/Garage/Assets/Scripts/Init.cs
@@ -2,38 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using SimpleJSON;
+using System.Text;
 
-[System.Serializable]
-public class JsonList
-{
-    public List<Init> data;
-
-    public static JsonList Parse(string json)
-    {
-        return JsonUtility.FromJson<JsonList>(json);
-    }
-}
 
 
 
 [System.Serializable]
 public class Init
 {
-    private string tofill;
-    public string id;
-    public float TotalFuel;
-    public float RemainingFuel;
-    public string Name;
-    public string Location;
-    public string Coordinates;
+    // resulting JSON from an API request
+    public JSONNode jsonResult;
 
     public string apiParam;
     
-
-
-
     // static string data = null;
-    private string apiUrl = "http://127.0.0.1:8000/";  //For now, hardcode the url with portnumber. Once kargame site is setup, fetch this url from there and update automatically
+    private string apiUrl = "http://192.168.0.100:8000/";
 
     
 
@@ -42,19 +26,19 @@ public class Init
         return JsonUtility.ToJson(this);
     }
 
-    public static Init Parse(string json)
+    private static Init Parse(string json)
     {
         return JsonUtility.FromJson<Init>(json);
     }
 
 
-    public IEnumerator Download(string id, System.Action<JsonList> callback = null)
+    public IEnumerator Download(string id, System.Action<JSONNode> callback = null)
     {
         using (UnityWebRequest request = UnityWebRequest.Get(apiUrl + id))
         {
             yield return request.SendWebRequest();
 
-            if (request.isNetworkError || request.isHttpError)
+            if ((request.result == UnityWebRequest.Result.ConnectionError) || (request.result == UnityWebRequest.Result.ProtocolError))
             {
                 // Debug.Log(request.error);
                 if (callback != null)
@@ -66,19 +50,52 @@ public class Init
             {
                 if (callback != null)
                 {
-                    callback.Invoke(JsonList.Parse(request.downloadHandler.text));
-                    // var jsondata = Init.Parse(request.downloadHandler.text);
-                    // Debug.Log("Response: "+request.downloadHandler.text);
-                    // Debug.Log(jsondata.TotalFuel);
-
-                    // JsonList loadedScoreData = JsonList.Parse(request.downloadHandler.text);
-                    // Debug.Log(loadedScoreData.data[0].RemainingFuel);
-                    // Debug.Log(loadedScoreData.data[0].Location);
-                    
+                    callback.Invoke(Processjson(request.downloadHandler.text));
                 }
             }
         }
     }
+
+
+
+    public IEnumerator Post(JSONObject jobject,System.Action<string> callback = null)
+    {
+        string postData = jobject.ToString();
+        var request = new UnityWebRequest(apiUrl+"playerprefspush", "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(postData);
+        request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+        if ((request.result == UnityWebRequest.Result.ConnectionError) || (request.result == UnityWebRequest.Result.ProtocolError))
+        {
+            Debug.Log(request.error);
+            if(callback != null) {
+                callback.Invoke(request.error);
+            }
+        }
+        else
+        {
+            // Debug.Log(request.downloadHandler.text);
+            if(callback != null) {
+                callback.Invoke(request.downloadHandler.text);
+            }
+            Debug.Log("Status code "+request.responseCode);
+        }
+
+    }
+
+    private JSONNode Processjson(string jsonString)
+     {
+         // parse the raw string into a json result we can easily read
+        jsonResult = JSON.Parse(jsonString);
+
+        // display the results on screen
+        // Debug.Log(jsonResult["data"][0]["RemainingFuel"]);
+
+        return jsonResult;
+     }
 
     
 }
