1c5d47a399c3b1d379319061e6269f7dd63f9d35
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2021-05-25 17:28:36 +0200

    translation project

---
+++ UNITY/Translation/Assets/Scripts/GameCenter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameCenter : MonoBehaviour {
+    
+    void Start () {
+        // Authenticate and register a ProcessAuthentication callback
+        // This call needs to be made before we can proceed to other calls in the Social API
+        Social.localUser.Authenticate (ProcessAuthentication);
+        StartCoroutine(KeepCheckingAvatar());
+    }
+
+    // This function gets called when Authenticate completes
+    // Note that if the operation is successful, Social.localUser will contain data from the server. 
+    void ProcessAuthentication (bool success) {
+        if (success) {
+            Debug.Log ("Authenticated, checking achievements");
+            Debug.Log(Social.Active.localUser.userName);
+            Debug.Log("UniqueID="+SystemInfo.deviceUniqueIdentifier);
+            Rect rect = new Rect (0, 0, Social.Active.localUser.image.width, Social.Active.localUser.image.width);
+            GetComponent<Image>().sprite = Sprite.Create(Social.Active.localUser.image, rect, new Vector2(0, 0));
+        }
+        else
+            Debug.Log ("Failed to authenticate");
+    
+    }
+
+    private IEnumerator KeepCheckingAvatar()
+    {
+        float secondsOfTrying = 10;
+        float secondsPerAttempt = 0.2f;
+        while (secondsOfTrying > 0)
+        {
+            
+            
+                Rect rect = new Rect (0, 0, Social.Active.localUser.image.width, Social.Active.localUser.image.width);
+                GetComponent<Image>().sprite = Sprite.Create(Social.Active.localUser.image, rect, new Vector2(0, 0));
+                Debug.Log(Social.Active.localUser.userName);
+                
+            
+
+            secondsOfTrying -= secondsPerAttempt;
+            yield return new WaitForSeconds(secondsPerAttempt);
+        }
+    }
+
+}
---
+++ UNITY/Translation/Assets/Scripts/PlayStore.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+
+public class PlayStore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            PlayGamesPlatform.Activate();
+        
+            StartCoroutine(KeepCheckingAvatar());
+            PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (result) =>{
+
+            Debug.Log("Username is"+Social.Active.localUser.userName);
+            Debug.Log("width is"+Social.Active.localUser.image.width);
+            Rect rect = new Rect (0, 0, Social.Active.localUser.image.width, Social.Active.localUser.image.width);
+            GetComponent<Image>().sprite = Sprite.Create(Social.Active.localUser.image, rect, new Vector2(0, 0));
+    });
+    
+}
+private IEnumerator KeepCheckingAvatar()
+    {
+        float secondsOfTrying = 10;
+        float secondsPerAttempt = 0.2f;
+        while (Social.Active.localUser.image == null)
+        {
+                yield return null;
+        }
+        Rect rect = new Rect (0, 0, Social.Active.localUser.image.width, Social.Active.localUser.image.width);
+        GetComponent<Image>().sprite = Sprite.Create(Social.Active.localUser.image, rect, new Vector2(0, 0));
+        Debug.Log(Social.Active.localUser.userName);
+    }
+    
+    }
