8da590d77a1519d1ad0d6eab114c6676457ab249
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2021-05-31 15:22:58 +0200

    mobile friendly MMO

--- UNITY/FPS_ViBe/Assets/HelloWorldManager.cs
+++ UNITY/FPS_ViBe/Assets/HelloWorldManager.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 
 public class HelloWorldManager : MonoBehaviour
     {
-        
+        void Start(){
+            Camera.main.GetComponent<CameraFollowController>().enabled = false;
+        }
         public void SH(){
+            Camera.main.GetComponent<CameraFollowController>().enabled = true;
             NetworkManager.Singleton.StartHost();
         }
         public void SC(){
+            Camera.main.GetComponent<CameraFollowController>().enabled = true;
             NetworkManager.Singleton.StartClient();
         }
         public void SS(){
+            Camera.main.GetComponent<CameraFollowController>().enabled = false;
             NetworkManager.Singleton.StartServer();
         }
 
