e4028f27b1a476d416d5ff6d69cf6b9fc0781960
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2021-06-06 05:39:00 +0200

    garage + classroom - might+day scene

--- UNITY/Garage-PerformanceOptimisation/Assets/Scripts/DayNight.cs
+++ UNITY/Garage-PerformanceOptimisation/Assets/Scripts/DayNight.cs
@@ -18,9 +18,16 @@ public class DayNight : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        if(int.Parse(DisplayTime.wordstime[1])<7 || int.Parse(DisplayTime.wordstime[1])>=17){
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+
+        }
+        else{
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        }
     }
 }
