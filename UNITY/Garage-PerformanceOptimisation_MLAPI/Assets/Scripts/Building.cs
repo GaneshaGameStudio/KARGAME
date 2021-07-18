e5a0d4eec2eaecd44a9aad2eb634fcd87304e6ba
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2021-07-18 06:00:44 +0200

    Landmarks buildings respawn on network

--- UNITY/Garage-PerformanceOptimisation_MLAPI/Assets/Scripts/Building.cs
+++ UNITY/Garage-PerformanceOptimisation_MLAPI/Assets/Scripts/Building.cs
@@ -1,15 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using MLAPI;
-using MLAPI.Messaging;
-using MLAPI.Spawning;
-using MLAPI.NetworkVariable;
-using MLAPI.Connection;
-public class Building : NetworkBehaviour
+public class Building : MonoBehaviour
 {   
     private string Vehicletemp;
-    private GameObject GOO;
     private Vector3 buildingcoord;
     private Vector3 buildingrot;
     private float deg2rad = 0.01745311f;
@@ -20,20 +16,29 @@ public class Building : NetworkBehaviour
     }
     private void OnTriggerEnter(Collider building)
     {   
+        
         if(building.transform.parent){
              if(building.transform.parent.tag=="2Wheeler"){
-            print("buildingentered");
-            GOO = GameObject.Find(VehicleID.Vehicle + "(Clone)");
-            buildingcoord = GOO.transform.position;
-            buildingrot = GOO.transform.localRotation.eulerAngles;
+            
             Vehicletemp = VehicleID.Vehicle;
-            Destroy(GOO);
+            PlayerPrefs.SetString("Vehicletemp", Vehicletemp);
             VehicleID.Vehicle = "Vibe2009rig-redoCSY";
-            //Instantiate(Resources.Load("Vehicles_prefabs/Vibe2009rig-redoCSY"), new Vector3(buildingcoord.x + 1.0f*Mathf.Sin(deg2rad*buildingrot.y),buildingcoord.y ,buildingcoord.z+ 1.0f*Mathf.Cos(deg2rad*buildingrot.y)), Quaternion.Euler(new Vector3(0f,buildingrot.y,0f)));
-            ulong id = NetworkManager.Singleton.LocalClientId;
-            SpawnServerRpc(id, VehicleID.Vehicle,buildingcoord.x + 1.0f*Mathf.Sin(deg2rad*buildingrot.y), buildingcoord.y ,buildingcoord.z+ 1.0f*Mathf.Cos(deg2rad*buildingrot.y), buildingrot.y);
-            //GOO = GameObject.Find(VehicleID.Vehicle + "(Clone)");
-            //CameraFollowController.objectToFollow = GOO.transform;
+            buildingcoord = building.gameObject.transform.parent.position;
+            buildingrot = building.gameObject.transform.parent.localRotation.eulerAngles;
+            PlayerPrefs.SetFloat("SpawnLoc.x",buildingcoord.x + 1.0f*Mathf.Sin(deg2rad*buildingrot.y));
+            PlayerPrefs.SetFloat("SpawnLoc.y",buildingcoord.y + 0.2f);
+            PlayerPrefs.SetFloat("SpawnLoc.z",buildingcoord.z + 1.0f*Mathf.Cos(deg2rad*buildingrot.y));
+
+            PlayerPrefs.SetFloat("SpawnRot.x",0f);
+            PlayerPrefs.SetFloat("SpawnRot.y",buildingrot.y);
+            PlayerPrefs.SetFloat("SpawnRot.z",0f);
+            PlayerPrefs.Save();
+            NetworkManager.Singleton.StopHost();
+            NetworkManager.Singleton.Shutdown();
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+            
+            
             }
         }
        
@@ -43,32 +48,28 @@ public class Building : NetworkBehaviour
     {  
         if(building.transform.parent){
             if(building.transform.parent.tag=="Manushya"){
-            print("buildingexit");
-            GOO = GameObject.Find(VehicleID.Vehicle + "(Clone)");
-            buildingcoord = GOO.transform.position;
-            buildingrot = GOO.transform.localRotation.eulerAngles;
-            VehicleID.Vehicle = Vehicletemp;
-            Destroy(GOO);
-            Instantiate(Resources.Load("Vehicles_prefabs/"+VehicleID.Vehicle), new Vector3(buildingcoord.x + 1.0f*Mathf.Sin(deg2rad*buildingrot.y),buildingcoord.y ,buildingcoord.z+ 1.0f*Mathf.Cos(deg2rad*buildingrot.y)), Quaternion.Euler(new Vector3(0f,buildingrot.y,0f)));
-            GOO = GameObject.Find(VehicleID.Vehicle + "(Clone)");
-            CameraFollowController.objectToFollow = GOO.transform;
+            
+            buildingcoord = building.transform.parent.position;
+            buildingrot = building.gameObject.transform.parent.localRotation.eulerAngles;
+            PlayerPrefs.SetFloat("SpawnLoc.x",buildingcoord.x + 1.0f*Mathf.Sin(deg2rad*buildingrot.y));
+            PlayerPrefs.SetFloat("SpawnLoc.y",buildingcoord.y);
+            PlayerPrefs.SetFloat("SpawnLoc.z",buildingcoord.z + 1.0f*Mathf.Cos(deg2rad*buildingrot.y));
+
+            PlayerPrefs.SetFloat("SpawnRot.x",0f);
+            PlayerPrefs.SetFloat("SpawnRot.y",buildingrot.y);
+            PlayerPrefs.SetFloat("SpawnRot.z",0f);
+            VehicleID.Vehicle = PlayerPrefs.GetString("Vehicletemp");
+            PlayerPrefs.Save();
+            NetworkManager.Singleton.StopHost();
+            NetworkManager.Singleton.Shutdown();
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+            
+            
             }
         }
         
 
-    }
-    [ServerRpc]
-    public void SpawnServerRpc(ulong id, string Vehicletype,float x, float y, float z, float yrot){
-        GameObject prefab = Resources.Load("Vehicles_prefabs/" + Vehicletype) as GameObject; 
-        GameObject go = Instantiate(prefab, new Vector3(x, y, z), Quaternion.Euler(new Vector3(0f, yrot, 0f)));
-        
-
-        go.GetComponent<NetworkObject>().SpawnAsPlayerObject(id);
-        go.GetComponent<NetworkObject>().ChangeOwnership(id);
-        //GetComponent<NetworkObject>().Despawn();
-        //Destroy(gameObject);
-
-        
     }
 
 }
