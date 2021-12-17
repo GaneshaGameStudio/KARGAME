a42ed619bf9642d3f06634bc7a240da3455f9019
Vi.Be <116117854+DjentleViBe@users.noreply.github.com>
2021-12-17 05:41:49 +0100

    Helper screen classroom

--- UNITY/Garage-PerformanceOptimisation_MLAPI/Assets/Scripts/OpenWebsite.cs
+++ UNITY/Garage-PerformanceOptimisation_MLAPI/Assets/Scripts/OpenWebsite.cs
@@ -5,10 +5,29 @@ using TMPro;
 using UnityEngine.EventSystems;
 
 public class OpenWebsite : MonoBehaviour
-{
+{   
+    private bool show;
+    public GameObject showobj;
+    void Start(){
+      //showobj = GameObject.Find("Helper");
+      show=true;
+    }
     public void btn_open_site (string websitename)
      {  
       //   Debug.Log(websitename);
         Application.OpenURL("http://www."+websitename);
+     }
+     public void btn_open_helper(){
+       if(show==true){
+         showobj.SetActive(true);
+         show=false;
+        
+       }
+       else if(show==false){
+         showobj.SetActive(false);
+         show=true;
+         
+       }
+       
      }
 }
