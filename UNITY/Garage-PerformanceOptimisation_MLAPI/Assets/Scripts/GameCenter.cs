196db0a7b59caabc7fd6f23b16c2d9ba1935a17e
A.Run <arundhaduti@gmail.com>
2022-01-06 21:35:37 +0530

    Combined play games and Game center scripts to GameCenter.cs

--- UNITY/Garage-PerformanceOptimisation_MLAPI/Assets/Scripts/GameCenter.cs
+++ UNITY/Garage-PerformanceOptimisation_MLAPI/Assets/Scripts/GameCenter.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_ANDROID
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+#endif
 using UnityEngine.SocialPlatforms;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameCenter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        #if UNITY_IOS
         Debug.Log(SystemInfo.operatingSystem);
         if(SystemInfo.operatingSystem.Contains("iOS")){
             // Authenticate and register a ProcessAuthentication callback
@@ -16,8 +22,38 @@ public class GameCenter : MonoBehaviour
         Social.localUser.Authenticate (ProcessAuthentication);
         StartCoroutine(KeepCheckingAvatarIOS());
         }
+        #endif
+
+        #if UNITY_ANDROID
+        Scene scene = SceneManager.GetActiveScene();
+        if(scene.name=="Classroom"){
+            Debug.Log(SystemInfo.operatingSystem);
+            StartCoroutine(KeepCheckingAvatar());
+        }
+        #endif
     
 }
+
+void OnEnable()
+    {
+        #if UNITY_ANDROID
+        Debug.Log(SystemInfo.operatingSystem);
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            PlayGamesPlatform.Activate();
+        
+            StartCoroutine(KeepCheckingAvatar());
+            PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (result) =>{
+
+            Debug.Log("Username is"+Social.Active.localUser.userName);
+            Debug.Log("width is"+Social.Active.localUser.image.width);
+            Rect rect = new Rect (0, 0, Social.Active.localUser.image.width, Social.Active.localUser.image.width);
+            GetComponent<Image>().sprite = Sprite.Create(Social.Active.localUser.image, rect, new Vector2(0, 0));
+    });
+        #endif
+    }
+
 
 // This function gets called when Authenticate completes
     // Note that if the operation is successful, Social.localUser will contain data from the server. 
@@ -33,6 +69,7 @@ public class GameCenter : MonoBehaviour
             Debug.Log ("Failed to authenticate");
     
     }
+    
 
     private IEnumerator KeepCheckingAvatarIOS()
     {
@@ -47,6 +84,19 @@ public class GameCenter : MonoBehaviour
             secondsOfTrying -= secondsPerAttempt;
             yield return new WaitForSeconds(secondsPerAttempt);
         }
+    }
+
+    private IEnumerator KeepCheckingAvatar()
+    {
+        float secondsOfTrying = 10;
+        float secondsPerAttempt = 0.2f;
+        while (Social.Active.localUser.image == null)
+        {
+                yield return null;
+        }
+        Rect rect = new Rect (0, 0, Social.Active.localUser.image.width, Social.Active.localUser.image.width);
+        GetComponent<Image>().sprite = Sprite.Create(Social.Active.localUser.image, rect, new Vector2(0, 0));
+        Debug.Log(Social.Active.localUser.userName);
     }
 
     }
